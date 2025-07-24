7f4110a53eaadc2b7920c36285ef6f2059b47481
herSugar <thereaperx008@gmail.com>
2025-07-24 18:36:07 +0800

    listdandetail

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+  public function index()
+    {
+        $products = Products::all();
+        return view('products.index', compact('products'));
+    }
+
+    public function show($id)
+    {
+        $product = Products::findOrFail($id);
+        return view('products.show', compact('product'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
