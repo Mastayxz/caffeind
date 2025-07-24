03fddb58e810958f44e33670ec0719e7e7a48a03
VianWeb <playinghd10@gmail.com>
2025-07-24 19:03:25 +0800

    FEAT: Dashboard Admin

--- routes/admin.php
+++ routes/admin.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 
-route::prefix('/admin/dashboard')->group(function() {
-    route::get('/', [AdminController::class, 'index'])->name('admin.dashboard');
+route::prefix('/admin')->group(function () {
+    route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
 });
