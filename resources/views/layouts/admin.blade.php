660e7014b3c5189d8584be84953fb7f6cb63fe10
imanuel22 <imanuelrenra220@gmail.com>
2025-07-25 08:39:35 +0800

    add alert

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body class="bg-gray-100">
     <div x-data="{ open: true }" class="flex h-screen" x-cloak x-init>
         @include('partials.sidebar')
@@ -12,12 +14,33 @@
         <div class="flex-1 flex flex-col">
             @include('partials.navbar')
             <main class="p-6">
+                @if (session('success'))
+                    <div class="bg-green-100 text-green-800 p-2 mb-4 rounded">
+                        {{ session('success') }}
+                    </div>
+                @endif
+
+                @if (session('error'))
+                    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4"
+                        role="alert">
+                        {{ session('error') }}
+                    </div>
+                @endif
+
+                @if ($errors->any())
+                    <div class="bg-red-100 text-red-800 p-2 mb-4 rounded">
+                        @foreach ($errors->all() as $error)
+                            <p>{{ $error }}</p>
+                        @endforeach
+                    </div>
+                @endif
                 @yield('content')
             </main>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 
-    <!-- Alpine.js -->
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
 </body>
+
 </html>
