8920d85ce56dee5ae34bb3f65b046e3d107d0821
Mastayxz <gustibagus826@gmail.com>
2025-07-25 10:22:17 +0800

    new masta

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -172,6 +172,7 @@
                                             <div>
                                                 <label class="block mb-2 text-sm font-medium text-gray-900">Upload
                                                     Image</label>
+
                                                 <div class="flex items-center justify-center w-full">
                                                     <label
                                                         class="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
